5430f1f2b6b28bb1e53d83c3686cf7a6be4454e6
DePoLK <ms0507030@gmail.com>
2020-02-20 22:31:45 +0800

    新增Json格式
    遊戲內存讀檔尚須製作

--- Assets/Scripts/CreateJsonData.cs
+++ Assets/Scripts/CreateJsonData.cs
@@ -3,81 +3,107 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 
-class CreateJsonData : MonoBehaviour {
 
-    CharaterData CD = new CharaterData();
+[System.Serializable]
+public class Charater
+{
+    public int id;
+    public string name;
+    public string Img;
+    public int Type;
+    public int Lv;
+    public int Exp;
+    public int MaxHp;
+    public int AtkBuffValue;
+    public int DefBuffValue;
+    public int MagicBuffValue;
+    public int HealBuffValue;
+    public int SpeedBuffValue;
 
-	
-    public void IniCharaterData()
+    public Charater(int _id, string _name, string _Img, int _Type, int _Lv, int _Exp, int _MaxHp, int _AtkBuffValue, int _DefBuffValue, int _MagicBuffValue, int _HealBuffValue, int _SpeedBuffValue)
     {
-        //[Header("希多爾數值")]
-        CD.HidoruNo = 1;
-        CD.HidoruImg = "Hidoru";
-        CD.HidoruType = 1;
-        CD.HidoruLv = 1;
-        CD.HidoruExp = 0;
-        CD.HidoruNextExp = 50;
-        CD.HidoruMaxHp = 150;
-        CD.HidoruAtkBuffValue = 15;
-        CD.HidoruDefBuffValue = 7;
-        CD.HidoruMagicBuffValue = 5;
-        CD.HidoruHealBuffValue = 8;
+        this.id = _id;
+        this.name = _name;
+        this.Img = _Img;
+        this.Type = _Type;
 
-        //[Header("奈爾數值")]
-        CD.NaiaNo = 2;
-        CD.NaiaImg = "Naia";
-        CD.NaiaType = 2;
-        CD.NaiaLv = 1;
-        CD.NaiaExp = 0;
-        CD.NaiaNextExp = 50;
-        CD.NaiaMaxHp = 220;
-        CD.NaiaAtkBuffValue = 10;
-        CD.NaiaDefBuffValue = 15;
-        CD.NaiaMagicBuffValue = 3;
-        CD.NaiaHealBuffValue = 5;
+        this.Lv = _Lv;
+        this.Exp = _Exp;
+        this.MaxHp = _MaxHp;
+        this.AtkBuffValue = _AtkBuffValue;
+        this.DefBuffValue = _DefBuffValue;
+        this.MagicBuffValue = _MagicBuffValue;
+        this.HealBuffValue = _HealBuffValue;
+        this.SpeedBuffValue = _SpeedBuffValue;
+    }
+    
 
-        //[Header("珂羅娜數值")]
-        CD.KoronaNo = 3;
-        CD.KoronaImg = "Korona";
-        CD.KoronaType = 3;
-        CD.KoronaLv = 1;
-        CD.KoronaExp = 0;
-        CD.KoronaNextExp = 50;
-        CD.KoronaMaxHp = 120;
-        CD.KoronaAtkBuffValue = 5;
-        CD.KoronaDefBuffValue = 5;
-        CD.KoronaMagicBuffValue = 20;
-        CD.KoronaHealBuffValue = 10;
-
-        //[Header("米亞莎數值")]
-        CD.MiasaNo = 4;
-        CD.MiasaImg = "Miasa";
-        CD.MiasaType = 4;
-        CD.MiasaLv = 1;
-        CD.MiasaExp = 0;
-        CD.MiasaNextExp = 50;
-        CD.MiasaMaxHp = 140;
-        CD.MiasaAtkBuffValue = 5;
-        CD.MiasaDefBuffValue = 6;
-        CD.MiasaMagicBuffValue = 8;
-        CD.MiasaHealBuffValue = 12;
+}
 
 
 
+public static class JsonHelper
+{
+    public static T[] FromJson<T>(string json)
+    {
+        Wrapper<T> wrapper = JsonUtility.FromJson<Wrapper<T>>(json);
+        return wrapper.Items;
+    }
 
+    public static string ToJson<T>(T[] array)
+    {
+        Wrapper<T> wrapper = new Wrapper<T>();
+        wrapper.Items = array;
+        return JsonUtility.ToJson(wrapper);
+    }
 
+    public static string ToJson<T>(T[] array, bool prettyPrint)
+    {
+        Wrapper<T> wrapper = new Wrapper<T>();
+        wrapper.Items = array;
+        return JsonUtility.ToJson(wrapper, prettyPrint);
+    }
 
-
-        // 暫存數據
-
-        string SaveString = JsonUtility.ToJson(CD);
-
-        // 輸出Json
-
-        StreamWriter file = new StreamWriter(System.IO.Path.Combine("Assets/GameData", "CharaterData.json"));
-
-        file.Write(SaveString);
-
-        file.Close();
+    [System.Serializable]
+    private class Wrapper<T>
+    {
+        public T[] Items;
     }
 }
+
+class CreateJsonData : MonoBehaviour {
+
+    
+
+    private void Start()
+    {
+        IniCharaterData();
+    }
+   
+    public  void IniCharaterData()
+    {
+        Charater[] CharaterUnJsonData = new Charater[4];
+
+        CharaterUnJsonData[0] = new Charater(0,"Hidoru","Assets/Sprites/CharaterImg/Hidoru_1.png",1,1,0,150,15,7,5,8,15);
+        CharaterUnJsonData[1] = new Charater(1, "Naia", "Assets/Sprites/CharaterImg/Naia_2.png", 2, 1,0, 220, 10, 15, 3, 5, 8);
+        CharaterUnJsonData[2] = new Charater(2, "Korona", "Assets/Sprites/CharaterImg/Korona_3.png", 3, 1,0, 120, 5, 5, 20, 10,10);
+        CharaterUnJsonData[3] = new Charater(3, "Miasa", "Assets/Sprites/CharaterImg/Miasa_4.png", 4, 1, 0, 140, 5, 6, 8, 12, 9);
+
+        string CharDataJson = JsonHelper.ToJson(CharaterUnJsonData, true); // 轉換Json
+       
+
+
+        //輸出Json
+        StreamWriter File = new StreamWriter(Application.dataPath + "/GameData/CharaterData.json");
+        File.Write(CharDataJson);
+        File.Close();
+        //輸出Json
+    }
+
+
+   
+
+}
+
+
+
---
+++ Assets/Scripts/DataManager.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using Pathfinding.Serialization.JsonFx;
+
+
+
+
+public class DataManager : MonoBehaviour{
+
+    [System.Serializable]
+    public class CharaterData
+    {
+        public int id;
+        public string name;
+        public string Img;
+        public int Type;
+        public int Lv;
+        public int Exp;
+        public int MaxHp;
+        public int AtkBuffValue;
+        public int DefBuffValue;
+        public int MagicBuffValue;
+        public int HealBuffValue;
+        public int SpeedBuffValue;
+    }
+
+    public static class JsonHelper
+    {
+        public static T[] FromJson<T>(string json)
+        {
+            Wrapper<T> wrapper = JsonUtility.FromJson<Wrapper<T>>(json);
+            return wrapper.Items;
+        }
+
+        public static string ToJson<T>(T[] array)
+        {
+            Wrapper<T> wrapper = new Wrapper<T>();
+            wrapper.Items = array;
+            return JsonUtility.ToJson(wrapper);
+        }
+
+        public static string ToJson<T>(T[] array, bool prettyPrint)
+        {
+            Wrapper<T> wrapper = new Wrapper<T>();
+            wrapper.Items = array;
+            return JsonUtility.ToJson(wrapper, prettyPrint);
+        }
+
+        [System.Serializable]
+        private class Wrapper<T>
+        {
+            public T[] Items;
+        }
+    } // 用以轉化多項目json
+
+    // Use this for initialization
+    void Start () {
+
+       //CJD.IniCharaterData(); //初始化Json
+
+       //初始讀取
+        FileStream fs = new FileStream(Application.dataPath + "/GameData/CharaterData.json", FileMode.Open);
+        StreamReader sr = new StreamReader(fs);
+        // PlayerPrefs.SetString("JsonData", sr.ReadToEnd());
+
+        string LoadJson = sr.ReadToEnd();
+
+        CharaterData[] GetLoadData = JsonHelper.FromJson<CharaterData>(LoadJson); // Json轉成物件
+
+        /*for(int i = 0; i < GetLoadData.Length; i++)
+        {
+            Debug.Log("id: " + GetLoadData[i].id);
+            Debug.Log("name: " + GetLoadData[i].name);
+            Debug.Log("Img: " + GetLoadData[i].Img);
+            Debug.Log("Type: " + GetLoadData[i].Type);
+            Debug.Log("Lv: " + GetLoadData[i].Lv);
+            Debug.Log("Exp: " + GetLoadData[i].Exp);
+            Debug.Log("MaxHp: " + GetLoadData[i].MaxHp);
+            Debug.Log("Atk: " + GetLoadData[i].AtkBuffValue);
+            Debug.Log("Def: " + GetLoadData[i].DefBuffValue);
+            Debug.Log("Magic: " + GetLoadData[i].MagicBuffValue);
+            Debug.Log("Heal: " + GetLoadData[i].HealBuffValue);
+            Debug.Log("Speed: " + GetLoadData[i].SpeedBuffValue);
+            Debug.Log("---Next_Charater---");
+
+
+        }*/
+
+
+
+
+
+        fs.Close();
+
+
+        
+        
+         
+
+        //初始讀取
+
+        
+
+        //Debug.Log(PlayerPrefs.GetString("JsonData"));
+        /*Debug.Log("ID: " +LoadCharData.id);
+        Debug.Log("Name: " +LoadCharData.name);
+        Debug.Log("Img: " +LoadCharData.Img);
+        Debug.Log("Type: " +LoadCharData.Type);
+        for(int i = 0;i < LoadCharData.Stats.Capacity; i++)
+        {
+            Debug.Log("Stats" + "[" + i + "]: " + LoadCharData.Stats[i]);
+        }*/
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+           
+        }//Save
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+
+           
+
+        }//Load
+
+	}
+
+
+    public void SaveData()
+    {
+
+    }
+
+    public void LoadData()
+    {
+
+    }
+
+
+}
+
